5eff3b3b0e261aca23861e75d6ec069f0fcd696f
tpyoung <tyleryoung@Tylers-MacBook-Pro.local>
2016-03-10 15:16:46 -1000

    Arrays Challege

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,63 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+//push
+var nameArray =[];
+nameArray.push("Tyler", "Young");
+
+genericNumberArray.push(6,7,8,9,10);
+
+
+//pop
+var popResult = colors.pop();
+
+//reverse
+scrambledWords.reverse();
+phoneNumber.reverse();
+
+//shift
+nextOrder = orderQueue.shift();
+
+//sort
+mixedNums.sort();
+mixedWords.sort();
+
+//splice
+var notFruit = fruitCollection.splice(1, 3);
+gemBox.splice(5, 2);
+gemBox.splice(6,4);
+upToTen.splice(2,0, 3,4,5,6,7,8);
+brownOnly.splice(1,5, 'brown','brown','brown');
+console.log(brownOnly);
+
+
+//unshift
+orderedValues.unshift(1, 2, 3);
+var orderedValuesNewLength = orderedValues.length;
+
+
+
+//Concat
+
+var randomThingsArray = genericNumberArray.concat(colors);
+var updatedOrders = orderQueue.concat({ takeOut: 'Ahi' }, { takeOut: 'Dank Memes'});
+
+//join
+var sentence = scrambledWords.join(" ");
+var myFruits = fruitCollection.join(" + ");
+
+//slice
+var favoriteFriends = friends.slice(1,-3);
+var owesMoney = friends.slice(3, 6);
+
+//toString
+var monthNameString = monthNames.toString();
+
+//indexOf
+var favoriteColor = colors.indexOf('Red');
+var favoriteEvenNumber = mixedNums.indexOf(76);
+
+//lastIndexOf
+var lastNine = bulkNumbers.lastIndexOf(9);
+var lastFive = bulkNumbers.lastIndexOf(5);
